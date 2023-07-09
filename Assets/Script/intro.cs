f39ba1b383ab83931996e06c445fdf2c2d535fd5
Dustfoxx <fox.hamren@gmail.com>
2023-07-09 15:13:46 +0200

    Lfs dump

---
+++ Assets/Script/intro.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intro : MonoBehaviour
+{
+    float timeLine;
+    [SerializeField] private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLine = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - timeLine > 3)
+        {
+            Instantiate(player, new Vector3(1.817f, -0.488f, 0f), new Quaternion(0,0,0,0));
+            Destroy(this.gameObject);
+        }
+    }
+}
